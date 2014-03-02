d14200399cd3999b00bfc615d89e88d0f04f115f
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-03-02 14:15:43 +0900

    Add spec

--- spec/kumogata_spec.rb
+++ spec/kumogata_spec.rb
@@ -64,6 +64,75 @@ end
     }.to raise_error('CODE: MESSAGE')
   end
 
+  it 'validate JSON template (without error)' do
+    template = <<-EOS
+{
+  "Resources": {
+    "myEC2Instance": {
+      "Type": "AWS::EC2::Instance",
+      "Properties": {
+        "ImageId": "ami-07f68106",
+        "InstanceType": "t1.micro"
+      }
+    }
+  },
+  "AZ": {
+    "Value": {
+      "Fn::GetAtt": [
+        "myEC2Instance",
+        "AvailabilityZone"
+      ]
+    }
+  }
+}
+    EOS
+
+    run_client(:validate, :template => template, :template_ext => '.template') do |client, cf|
+      json = JSON.parse(template).to_json
+
+      cf.should_receive(:validate_template).with(json) {
+        {}
+      }
+    end
+  end
+
+  it 'validate JSON template (with error)' do
+    template = <<-EOS
+{
+  "Resources": {
+    "myEC2Instance": {
+      "Type": "AWS::EC2::Instance",
+      "Properties": {
+        "ImageId": "ami-07f68106",
+        "InstanceType": "t1.micro"
+      }
+    }
+  },
+  "AZ": {
+    "Value": {
+      "Fn::GetAtt": [
+        "myEC2Instance",
+        "AvailabilityZone"
+      ]
+    }
+  }
+}
+    EOS
+
+    expect {
+      run_client(:validate, :template => template, :template_ext => '.template') do |client, cf|
+        json = JSON.parse(template).to_json
+
+        cf.should_receive(:validate_template).with(json) {
+          {
+            :code => 'CODE',
+            :message => 'MESSAGE'
+          }
+        }
+      end
+    }.to raise_error('CODE: MESSAGE')
+  end
+
   it 'convert Ruby template to JSON template' do
     template = <<-EOS
 Resources do
