99b92dd79c1636920abd02dec1cdf9761a2d7b3a
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-03-03 02:38:43 +0900

    Add list spec

--- spec/kumogata_list_spec.rb
+++ spec/kumogata_list_spec.rb
@@ -17,24 +17,7 @@ describe 'Kumogata::Client#list' do
         obj.should_receive(:description) { nil }
       end
 
-      stacks = make_double('stacks') do |obj|
-        obj.should_receive(:map).and_yield(stack1).and_yield(stack2).and_return([
-          {
-            "StackName"    => "stack1",
-            "CreationTime" => "2014-03-02 16:17:18 UTC",
-            "StackStatus"  => "CREATE_COMPLETE",
-            "Description"  => nil,
-          },
-          {
-            "StackName"    => "stack2",
-            "CreationTime" => "2014-03-02 16:17:19 UTC",
-            "StackStatus"  => "CREATE_COMPLETE",
-            "Description"  => nil,
-          }
-        ])
-      end
-
-      cf.should_receive(:stacks) { stacks }
+      cf.should_receive(:stacks) { [stack1, stack2] }
     end
 
     expect(json).to eq((<<-EOS).chomp)
@@ -54,4 +37,35 @@ describe 'Kumogata::Client#list' do
 ]
     EOS
   end
+
+  it 'list a specified stack' do
+    json = run_client(:list, :arguments => ['stack1']) do |client, cf|
+      stacks = double('status')
+
+      stack1 = make_double('stack1') do |obj|
+        obj.should_receive(:name).twice { 'stack1' }
+        obj.should_receive(:creation_time) { '2014-03-02 16:17:18 UTC' }
+        obj.should_receive(:status) { 'CREATE_COMPLETE' }
+        obj.should_receive(:description) { nil }
+      end
+
+      stack2 = make_double('stack2') do |obj|
+        obj.should_receive(:name) { 'stack2' }
+      end
+
+      cf.should_receive(:stacks) { [stack1, stack2] }
+    end
+
+    expect(json).to eq((<<-EOS).chomp)
+[
+  {
+    "StackName": "stack1",
+    "CreationTime": "2014-03-02 16:17:18 UTC",
+    "StackStatus": "CREATE_COMPLETE",
+    "Description": null
+  }
+]
+    EOS
+  end
+
 end
