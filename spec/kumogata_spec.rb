a7c293826a55eb7e91cad4559929f98d6fcbe93a
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-03-02 15:18:02 +0900

    Add spec

--- spec/kumogata_spec.rb
+++ spec/kumogata_spec.rb
@@ -58,6 +58,73 @@ end
     end
   end
 
+  it 'create a stack from Ruby template with parameters' do
+    template = <<-EOS
+Parameters do
+  InstanceType do
+    Default "t1.micro"
+    Description "Instance Type"
+    Type "String"
+  end
+end
+
+Resources do
+  myEC2Instance do
+    Type "AWS::EC2::Instance"
+    Properties do
+      ImageId "ami-XXXXXXXX"
+      InstanceType { Ref "InstanceType" }
+    end
+  end
+end
+
+Outputs do
+  AZ do
+    Value do
+      Fn__GetAtt "myEC2Instance", "AvailabilityZone"
+    end
+  end
+end
+    EOS
+
+    run_client(:create, :template => template, :options => {:parameters => ['InstanceType=m1.large']}) do |client, cf|
+      json = eval_template(template, :update_deletion_policy => true).to_json
+      stacks = double('status')
+
+      output = make_double('output') do |obj|
+        obj.should_receive(:key) { 'AZ' }
+        obj.should_receive(:value) { 'ap-northeast-1b' }
+      end
+
+      resource_summary = make_double('resource_summary') do |obj|
+        obj.should_receive(:[]).with(:logical_resource_id) { 'myEC2Instance' }
+        obj.should_receive(:[]).with(:physical_resource_id) { 'i-XXXXXXXX' }
+        obj.should_receive(:[]).with(:resource_type) { 'AWS::EC2::Instance' }
+        obj.should_receive(:[]).with(:resource_status) { 'CREATE_COMPLETE' }
+        obj.should_receive(:[]).with(:resource_status_reason) { nil }
+        obj.should_receive(:[]).with(:last_updated_timestamp) { '2014-03-02 04:35:12 UTC' }
+      end
+
+      stack = make_double('stack') do |obj|
+        obj.should_receive(:status).and_return(
+            'CREATE_COMPLETE', 'CREATE_COMPLETE',
+            'DELETE_COMPLETE', 'DELETE_COMPLETE', 'DELETE_COMPLETE')
+        obj.should_receive(:outputs) { [output] }
+        obj.should_receive(:resource_summaries) { [resource_summary] }
+        obj.should_receive(:delete)
+      end
+
+      stacks = make_double('status') do |obj|
+        obj.should_receive(:create)
+           .with('kumogata-XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', json, {:parameters=>{"InstanceType"=>"m1.large"}}) { stack }
+        obj.should_receive(:[])
+           .with('kumogata-XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX') { stack }
+      end
+
+      cf.should_receive(:stacks).twice { stacks }
+    end
+  end
+
   it 'create a stack from Ruby template with stack name' do
     template = <<-EOS
 Resources do
