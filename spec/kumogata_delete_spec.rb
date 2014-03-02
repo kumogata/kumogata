90c5510cd722d2c566a78e762d50b44e30bdda3c
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-03-02 15:37:24 +0900

    Add delete spec

---
+++ spec/kumogata_delete_spec.rb
@@ -0,0 +1,20 @@
+describe 'Kumogata::Client#delete' do
+  it 'update a stack from Ruby template' do
+    run_client(:delete, :arguments => ['MyStack'], :options => {:force => true}) do |client, cf|
+      stacks = double('status')
+
+      stack = make_double('stack') do |obj|
+        obj.should_receive(:delete).with(no_args())
+        obj.should_receive(:status).and_return(
+            'DELETE_COMPLETE', 'DELETE_COMPLETE', 'DELETE_COMPLETE')
+      end
+
+      stacks = make_double('stacks') do |obj|
+        obj.should_receive(:[])
+           .with('MyStack') { stack }
+      end
+
+      cf.should_receive(:stacks) { stacks }
+    end
+  end
+end
