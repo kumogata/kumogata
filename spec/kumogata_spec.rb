17e9e140261ffa6f0330d4db1d6104952a35735e
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-03-02 15:06:37 +0900

    Add spec

--- spec/kumogata_spec.rb
+++ spec/kumogata_spec.rb
@@ -5,7 +5,7 @@ Resources do
   myEC2Instance do
     Type "AWS::EC2::Instance"
     Properties do
-      ImageId "ami-07f68106"
+      ImageId "ami-XXXXXXXX"
       InstanceType "t1.micro"
     end
   end
@@ -40,8 +40,8 @@ end
 
       stack = make_double('stack') do |obj|
         obj.should_receive(:status).and_return(
-            'CREATE_COMPLETE', 'CREATE_COMPLETE', 'CREATE_COMPLETE', 'CREATE_COMPLETE',
-            'DELETE_COMPLETE')
+            'CREATE_COMPLETE', 'CREATE_COMPLETE',
+            'DELETE_COMPLETE', 'DELETE_COMPLETE', 'DELETE_COMPLETE')
         obj.should_receive(:outputs) { [output] }
         obj.should_receive(:resource_summaries) { [resource_summary] }
         obj.should_receive(:delete)
@@ -58,13 +58,69 @@ end
     end
   end
 
+  it 'create a stack from Ruby template with stack name' do
+    template = <<-EOS
+Resources do
+  myEC2Instance do
+    Type "AWS::EC2::Instance"
+    Properties do
+      ImageId "ami-XXXXXXXX"
+      InstanceType "t1.micro"
+    end
+  end
+end
+
+Outputs do
+  AZ do
+    Value do
+      Fn__GetAtt "myEC2Instance", "AvailabilityZone"
+    end
+  end
+end
+    EOS
+
+    run_client(:create, :arguments => ['MyStack'], :template => template) do |client, cf|
+      json = eval_template(template).to_json
+      stacks = double('status')
+
+      output = make_double('output') do |obj|
+        obj.should_receive(:key) { 'AZ' }
+        obj.should_receive(:value) { 'ap-northeast-1b' }
+      end
+
+      resource_summary = make_double('resource_summary') do |obj|
+        obj.should_receive(:[]).with(:logical_resource_id) { 'myEC2Instance' }
+        obj.should_receive(:[]).with(:physical_resource_id) { 'i-XXXXXXXX' }
+        obj.should_receive(:[]).with(:resource_type) { 'AWS::EC2::Instance' }
+        obj.should_receive(:[]).with(:resource_status) { 'CREATE_COMPLETE' }
+        obj.should_receive(:[]).with(:resource_status_reason) { nil }
+        obj.should_receive(:[]).with(:last_updated_timestamp) { '2014-03-02 04:35:12 UTC' }
+      end
+
+      stack = make_double('stack') do |obj|
+        obj.should_receive(:status).and_return(
+            'CREATE_COMPLETE',
+            'CREATE_COMPLETE')
+        obj.should_receive(:outputs) { [output] }
+        obj.should_receive(:resource_summaries) { [resource_summary] }
+      end
+
+      stacks = make_double('status') do |obj|
+        obj.should_receive(:create)
+           .with('MyStack', json, {}) { stack }
+      end
+
+      cf.should_receive(:stacks) { stacks }
+    end
+  end
+
   it 'validate Ruby template (without error)' do
     template = <<-EOS
 Resources do
   myEC2Instance do
     Type "AWS::EC2::Instance"
     Properties do
-      ImageId "ami-07f68106"
+      ImageId "ami-XXXXXXXX"
       InstanceType "t1.micro"
     end
   end
@@ -94,7 +150,7 @@ Resources do
   myEC2Instance do
     Type "AWS::EC2::Instance"
     Properties do
-      ImageId "ami-07f68106"
+      ImageId "ami-XXXXXXXX"
       InstanceType "t1.micro"
     end
   end
@@ -130,7 +186,7 @@ end
     "myEC2Instance": {
       "Type": "AWS::EC2::Instance",
       "Properties": {
-        "ImageId": "ami-07f68106",
+        "ImageId": "ami-XXXXXXXX",
         "InstanceType": "t1.micro"
       }
     }
@@ -162,7 +218,7 @@ end
     "myEC2Instance": {
       "Type": "AWS::EC2::Instance",
       "Properties": {
-        "ImageId": "ami-07f68106",
+        "ImageId": "ami-XXXXXXXX",
         "InstanceType": "t1.micro"
       }
     }
@@ -198,7 +254,7 @@ Resources do
   myEC2Instance do
     Type "AWS::EC2::Instance"
     Properties do
-      ImageId "ami-07f68106"
+      ImageId "ami-XXXXXXXX"
       InstanceType "t1.micro"
     end
   end
@@ -221,7 +277,7 @@ end
     "myEC2Instance": {
       "Type": "AWS::EC2::Instance",
       "Properties": {
-        "ImageId": "ami-07f68106",
+        "ImageId": "ami-XXXXXXXX",
         "InstanceType": "t1.micro"
       }
     }
