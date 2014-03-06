939ebfccf00dcc601c2702419fabdd6934469d37
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-03-06 10:45:14 +0900

    Fix String#undent

--- lib/kumogata/ext/string_ext.rb
+++ lib/kumogata/ext/string_ext.rb
@@ -30,8 +30,13 @@ class String
   end
 
   def undent
-    min_space_num = self.split("\n").delete_if{|s| s=~ /^\s*$/ }.map{|s| s[/^\s+/].length }.min
-    gsub(/^[ \t]{,#{min_space_num}}/, '')
+    min_space_num = self.split("\n").delete_if {|s| s =~ /^\s*$/ }.map {|s| (s[/^\s+/] || '').length }.min
+
+    if min_space_num and min_space_num > 0
+      gsub(/^[ \t]{,#{min_space_num}}/, '')
+    else
+      self
+    end
   end
 
   def fn_join(options = {})
