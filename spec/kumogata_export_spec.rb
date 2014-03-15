8a276de24783f745dbb000a8c8261855e04c8792
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-03-15 18:23:19 +0900

    Add spec

--- spec/kumogata_export_spec.rb
+++ spec/kumogata_export_spec.rb
@@ -56,4 +56,67 @@ Outputs do
 end
     EOS
   end
+
+  it 'export a JSON template' do
+    json = <<-EOS
+{
+  "Resources": {
+    "myEC2Instance": {
+      "Type": "AWS::EC2::Instance",
+      "Properties": {
+        "ImageId": "ami-XXXXXXXX",
+        "InstanceType": "t1.micro"
+      }
+    }
+  },
+  "Outputs": {
+    "AZ": {
+      "Value": {
+        "Fn::GetAtt": [
+          "myEC2Instance",
+          "AvailabilityZone"
+        ]
+      }
+    }
+  }
+}
+    EOS
+
+    template = run_client(:export, :arguments => ['MyStack'], :options => {:format => :json}) do |client, cf|
+      stack = make_double('stack') do |obj|
+        obj.should_receive(:status) { 'CREATE_COMPLETE' }
+        obj.should_receive(:template) { json }
+      end
+
+      stacks = make_double('stacks') do |obj|
+        obj.should_receive(:[]).with('MyStack') { stack }
+      end
+
+      cf.should_receive(:stacks) { stacks }
+    end
+
+    expect(template).to eq((<<-EOS).chomp)
+{
+  "Resources": {
+    "myEC2Instance": {
+      "Type": "AWS::EC2::Instance",
+      "Properties": {
+        "ImageId": "ami-XXXXXXXX",
+        "InstanceType": "t1.micro"
+      }
+    }
+  },
+  "Outputs": {
+    "AZ": {
+      "Value": {
+        "Fn::GetAtt": [
+          "myEC2Instance",
+          "AvailabilityZone"
+        ]
+      }
+    }
+  }
+}
+    EOS
+  end
 end
