9b39c67421b4ab54b2752487e008c342a31ffcc9
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-07-15 21:32:51 +0900

    Add spec

--- spec/kumogata_convert_spec.rb
+++ spec/kumogata_convert_spec.rb
@@ -126,6 +126,105 @@ end
     EOS
   end
 
+  it 'convert Ruby template to JavaScript template' do
+    template = <<-EOS
+Resources do
+  myEC2Instance do
+    Type "AWS::EC2::Instance"
+    Properties do
+      ImageId "ami-XXXXXXXX"
+      InstanceType "t1.micro"
+    end
+  end
+end
+
+Outputs do
+  AZ do
+    Value do
+      Fn__GetAtt "myEC2Instance", "AvailabilityZone"
+    end
+  end
+end
+    EOS
+
+    js_template = run_client(:convert, :template => template, :options => {:output_format => :js})
+
+    expect(js_template).to eq <<-EOS.strip
+({
+  "Resources": {
+    "myEC2Instance": {
+      "Type": "AWS::EC2::Instance",
+      "Properties": {
+        "ImageId": "ami-XXXXXXXX",
+        "InstanceType": "t1.micro"
+      }
+    }
+  },
+  "Outputs": {
+    "AZ": {
+      "Value": {
+        "Fn::GetAtt": [
+          "myEC2Instance",
+          "AvailabilityZone"
+        ]
+      }
+    }
+  }
+})
+    EOS
+  end
+
+  it 'convert JavaScript template to Ruby template' do
+    template = <<-EOS
+function fetch_ami() {
+  return "ami-XXXXXXXX";
+}
+
+({
+  Resources: { /* comment */
+    myEC2Instance: {
+      Type: "AWS::EC2::Instance",
+      Properties: {
+        ImageId: fetch_ami(),
+        InstanceType: "t1.micro"
+      }
+    }
+  },
+  Outputs: {
+    AZ: { /* comment */
+      Value: {
+        "Fn::GetAtt": [
+          "myEC2Instance",
+          "AvailabilityZone"
+        ]
+      }
+    }
+  }
+})
+    EOS
+
+    ruby_template = run_client(:convert, :template => template, :template_ext => '.js', :options => {:output_format => :ruby})
+
+    expect(ruby_template).to eq((<<-EOS).chomp)
+Resources do
+  myEC2Instance do
+    Type "AWS::EC2::Instance"
+    Properties do
+      ImageId "ami-XXXXXXXX"
+      InstanceType "t1.micro"
+    end
+  end
+end
+Outputs do
+  AZ do
+    Value do
+      Fn__GetAtt "myEC2Instance", "AvailabilityZone"
+    end
+  end
+end
+    EOS
+  end
+
   it 'convert YAML template to JSON template' do
     template = <<-EOS
 ---
